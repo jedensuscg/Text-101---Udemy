b8acada996afa64996de95548a9952fc77837e47
James Edens <jedensuscg@gmail.com>
2020-05-10 17:58:05 -0400

    Finished State Machine. Added first three Story Scenes.

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -1,22 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class AdventureGame : MonoBehaviour
 {
-    [SerializeField] State state;
+    [SerializeField] State startingState;
     [SerializeField] Text textComponent;
+ 
+
+    State state;
 
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = state.;
+        state = startingState;
+        textComponent.text = state.GetStateStory();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ManagedState();
+    }
+
+    private void ManagedState()
+    {
+        var nextStates = state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        textComponent.text = state.GetStateStory();
     }
 }
